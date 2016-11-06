6aa2252f9d5ebdfefa97f75e7e5ebd48b6abd561
sharipkanov <sharipkanov@gmail.com>
2016-11-06 19:41:40 +0600

    Think thats it

--- app/js/app.js
+++ app/js/app.js
@@ -24,5 +24,31 @@ LHFORUM.prototype.imageHalfSize = function () {
         var app = new LHFORUM;
 
         $('[data-phone-mask]').mask('+7 (000) 000-0000');
+
+        setTimeout(function() {
+            $('.s-switch').find('li').removeClass('uk-active');
+            $('.s-switch').find('li').first().addClass('uk-active');
+        }, 300);
+
+        $('.uk-form').submit(function(e) {
+            e.preventDefault();
+
+            var modal = UIkit.modal("#success-modal");
+
+            modal.show();
+
+            return false;
+        });
+
+        $('.buy-btn').click(function(e) {
+            e.preventDefault();
+
+            $('.vip-place-grid').fadeOut(300);
+            setTimeout(function() {
+                $('.paymanent').fadeIn(300);
+            }, 300);
+
+            return false;
+        });
     });
 })(jQuery);
